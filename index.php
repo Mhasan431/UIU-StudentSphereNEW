623b842b77724b460e49ed630050f055ad25126d
Mahadi Hasan <mhasan201431@bscse.uiu.ac.bd>
2023-07-12 02:00:45 +0600

    Index.php updated with navbar, header and footer

---
+++ index.php
@@ -0,0 +1,15 @@
+
+  <!-- Include header.php -->
+  <?php include 'header.php'; ?>
+
+  <!-- Include navbar.php -->
+  <?php include 'navbar.php'; ?>
+  
+  <!-- Content -->
+  <div class="container">
+    <h1>Welcome to Your Website</h1>
+    <p>This is the content of your website.</p>
+  </div>
+
+    <!-- Include footer.php -->
+    <?php include 'footer.php'; ?>
