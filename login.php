48c69742fc54885faa0cb2e3619811cf4a15a1d1
sabiha415 <usabiha201415@bscse.uiu.ac.bd>
2023-07-11 19:20:30 +0600

    header and footer added

--- login.php
+++ login.php
@@ -1,17 +1,26 @@
+<!-- Include header.php -->
+<?php include 'header.php'; ?>
+
+<!-- Include navbar.php -->
+<?php include 'navbar.php'; ?>
+
 <div class="container">
-    <h1>Login</h1>
+  <h1>Login</h1>
 
-    <form action="login.php" method="POST">
-      <div class="form-group">
-        <label for="email">Email:</label>
-        <input type="email" class="form-control" id="email" name="email" required>
-      </div>
-      <div class="form-group">
-        <label for="password">Password:</label>
-        <input type="password" class="form-control" id="password" name="password" required>
-      </div>
-      <button type="submit" class="btn btn-primary">Login</button>
-    </form>
+  <form action="login.php" method="POST">
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" name="email" required>
+    </div>
+    <div class="form-group">
+      <label for="password">Password:</label>
+      <input type="password" class="form-control" id="password" name="password" required>
+    </div>
+    <button type="submit" class="btn btn-primary">Login</button>
+  </form>
 
-    <p>Don't have an account? <a href="signup.php">Sign up</a> now.</p>
-  </div>
+  <p>Don't have an account? <a href="signup.php">Sign up</a> now.</p>
+</div>
+
+<!-- Include footer.php -->
+<?php include 'footer.php'; ?>
