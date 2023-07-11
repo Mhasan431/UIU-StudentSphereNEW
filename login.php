e37c2fb40e2a71e61356311d4aaa6f640fdef54b
sabiha415 <usabiha201415@bscse.uiu.ac.bd>
2023-07-11 19:18:34 +0600

    login file created

---
+++ login.php
@@ -0,0 +1,17 @@
+<div class="container">
+    <h1>Login</h1>
+
+    <form action="login.php" method="POST">
+      <div class="form-group">
+        <label for="email">Email:</label>
+        <input type="email" class="form-control" id="email" name="email" required>
+      </div>
+      <div class="form-group">
+        <label for="password">Password:</label>
+        <input type="password" class="form-control" id="password" name="password" required>
+      </div>
+      <button type="submit" class="btn btn-primary">Login</button>
+    </form>
+
+    <p>Don't have an account? <a href="signup.php">Sign up</a> now.</p>
+  </div>
