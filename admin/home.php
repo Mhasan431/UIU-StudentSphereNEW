474fc9c91fd8db4ec6fe3fffc849bbed7894e931
Mahadi Hasan <mhasan201431@bscse.uiu.ac.bd>
2023-08-28 03:06:16 +0600

    Complete

---
+++ admin/home.php
@@ -0,0 +1,24 @@
+
+<style>
+
+img{
+
+    height: 500px;
+    width: 900px;
+    margin: 50px 200px;
+}
+
+</style>
+
+
+<br>
+<hr><hr>
+<h1 style="text-align: center;">
+
+Welcome back Admin!!
+
+</h1>
+<hr><hr>
+
+
+<img  src="welcome.jpg" alt="">
